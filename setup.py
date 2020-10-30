cb6ef76d17989b4e6cb48d01c555f22e24bb3a3f
bhamacher <b.hamacher@zera.de>
2020-10-30 14:08:55 +0100

    Preparing repo for python project.
    .gitignore: ignore all project and gernrated files
    setup.py: build and install code
    CMakeList.txt: import in superbuild/qt Creator. Support build and install steps
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pythonconverter_pkg", 
+    version="0.0.1",
+    author="Bastian Hamacher",
+    author_email="b.hamacher@zera.de",
+    description="Zera mobile device report generator",
+    scripts=['ZeraConverter.py'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZeraGmbH/python-converter",
+    zip_safe=False,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
