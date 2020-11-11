8e79819817923142584bbb396e03a3dc7919ef89
bhamacher <b.hamacher@zera.de>
2020-11-11 08:11:26 +0100

    Revert "Removed tkinter gui."
    Avoid merge conflicts doing rebasing with master but keeping the gui.
    
    This reverts commit d756362b11b968b5062fd33d12111cac792f8089.

--- ZeraConverter.py
+++ ZeraConverter.py
@@ -2,7 +2,9 @@
 
 #Systemmodules
 import sys, getopt
-
+import tkinter as tk
+import tkinter.filedialog
+from tkinter.messagebox import showinfo
 
 #custom imports
 from pythonconverter_pkg import ConversionUnit as con
@@ -15,6 +17,7 @@ def enum(**enums):
 
 useDef = enum(Help=0, ShowRecords=2, ShowTransactions=3, Convert=4)
 
+root= tk.Tk()
 
 inputFile = ""
 outputFile = ""
@@ -28,6 +31,81 @@ converter = object
 
 
 
+def guiApp():
+	inputTb=""
+	outputTb=""
+	engineTb=""
+	sessionTb=""
+	def inputfileSearch():
+		global inputFile
+		inputFile=tk.filedialog.askopenfilename()
+		inputTb.delete(1.0,"end")
+		inputTb.insert(1.0,inputFile)
+	def outputfileSearch():
+		global outputFile
+		filename=tk.filedialog.asksaveasfile()
+		outputFile=filename.name
+		outputTb.delete(1.0,"end")
+		outputTb.insert(1.0,outputFile)
+	def enginefileSearch():
+		global userscript
+		userscript=tk.filedialog.askopenfilename()
+		engineTb.delete(1.0,"end")
+		engineTb.insert(1.0,userscript)
+	def findRecords():
+		global record
+		global converter
+		converter.setInputFile(inputFile)
+		ret = converter.ShowRecords()
+		i=0
+		for ele in ret :
+			showinfo("Sessions",ele[0])
+			print(i,": ",ele[0])
+			i=i+1
+
+	def convert():
+		global record
+		record=sessionTb.get(1.0,"end").rstrip('\n')
+		record
+		root.destroy()
+		
+
+
+	inputl=tk.Label(root,text="Database")
+	inputTb=tk.Text(root,height=1,width=60)
+	inputSb=tk.Button(root,text="search", command=inputfileSearch)
+	outputl=tk.Label(root,text="export file path")
+	outputTb=tk.Text(root,height=1,width=60)
+	outputSb=tk.Button(root,text="search",command=outputfileSearch)
+	sessionl=tk.Label(root,text="session")
+	sessionTb=tk.Text(root,height=1,width=60)
+	sessionSb=tk.Button(root,text="sessions",command=findRecords)
+	enginel=tk.Label(root,text="conversion engine")
+	engineTb=tk.Text(root,height=1,width=60)
+	engineSb=tk.Button(root,text="search",command=enginefileSearch)
+	convertB=tk.Button(root,text="CONVERT",command=convert)
+
+	inputl.grid(row=0)
+	inputTb.grid(row=1,column=0)
+	inputSb.grid(row=1,column=1)
+	outputl.grid(row=2)
+	outputTb.grid(row=3,column=0)
+	outputSb.grid(row=3,column=1)
+	sessionl.grid(row=4)
+	sessionTb.grid(row=5,column=0)
+	sessionSb.grid(row=5,column=1)
+	enginel.grid(row=6)
+	engineTb.grid(row=7,column=0)
+	engineSb.grid(row=7,column=1)
+	convertB.grid(row=8)
+
+	inputTb.insert(1.0,inputFile)
+	outputTb.insert(1.0,outputFile)
+	engineTb.insert(1.0,userscript)
+	sessionTb.insert(1.0,record)
+
+	root.mainloop()
+
 
 
 
@@ -40,7 +118,8 @@ def Help():
 	print(" ./Converter -i <file>.db")
 	print("Convert file")
 	print("	./Convert -i <file>.db -o <file>.xml --session=<sessionName>")
-
+	print("Start with gui")
+	print("	./Converter -g")
 
 
 def main(argv):
@@ -65,6 +144,8 @@ def main(argv):
 		if opt == '-h':
 			print('test.py -i <inputfile> -o <outputfile>')
 			sys.exit()
+		elif opt == "-g":
+			gui = True
 		elif opt in ("-i", "--ifile"):
 			inputFile = arg
 		elif opt in ("-o", "--ofile"):
@@ -74,10 +155,13 @@ def main(argv):
 		elif opt == "-d":
                         inputFile = "./test/test.db"
                         outputFile = "./test/out.xml"
+                        #userscript= "./zeraconverterengines/MTVisRes.py"
                         record= "[customer ID is not set] 2020/10/27"
-                        userscript= "zeraconverterengines.MTVisRes"
+                        userscript= "zeraconverterengines.MTVisRes2"
 
 		converter = con.ConversionUnit()
+	if gui == True:
+		guiApp()
 
 	print('Input file is ', inputFile)
 	print('Output file is ', outputFile)
