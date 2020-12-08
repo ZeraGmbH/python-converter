11983676b468e611df39f927be6098fdee1cf860
bhamacher <b.hamacher@zera.de>
2020-12-08 11:30:47 +0100

    Increase robustness.
    If one transaction failes to convert, the application will still try to
    convert the other transactions. But the convert function will return
    false and issue a warning.
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- pythonconverter_pkg/ConversionUnit.py
+++ pythonconverter_pkg/ConversionUnit.py
@@ -23,15 +23,19 @@ class ConversionUnit:
 
 	def setInputFile(self, inputFile):
 		self.__inputFile=inputFile
+		return True
 
 	def setOutputFile(self, outputFile):
 		self.__outputFile=outputFile
+		return True
 
 	def setType(self, conType):
 		self.__conType = conType
+		return True
 
 	def setConFile(self, conFile):
 		self.__conFile = conFile
+		return True
 
 	def seteParam(self,eparams):
 		try:
@@ -39,6 +43,7 @@ class ConversionUnit:
 			self.__eparameter = json.loads(json_acceptable_string)
 		except:
 			self.__eparameter=dict()
+		return True
 	
 	def setUserScript(self,file):
 		retVal=True
@@ -65,15 +70,19 @@ class ConversionUnit:
 		return ret
 
 	def convert(self,p_session):
-		self.__session = p_session
-		self.__iInt.openDatabase(self.__inputFile)
-		self.__oInt.openDatabase(self.__outputFile)
-		self.__read()
-		#print(self.__iMap)
-		self.__manipulateSet()
-		print(self.__oMap)
-		self.__write()
-		self.__iInt.closeDatabase()
+		retVal=True
+		try:
+			self.__session = p_session
+			self.__iInt.openDatabase(self.__inputFile)
+			self.__oInt.openDatabase(self.__outputFile)
+			self.__read()
+			retVal=self.__manipulateSet()
+			#print(self.__oMap)
+			self.__write()
+			self.__iInt.closeDatabase()
+		except:
+			retVal=False
+		return retVal
 
 	def __readTransactionList(self):
 		return self.__iInt.readDatasetList(self.__session)
@@ -102,12 +111,14 @@ class ConversionUnit:
 	
 	# manipulate input dataset (dict)
 	def __manipulateSet(self):
+		retVal=True
 		if self.__userScriptPath != "" :
 			manUnit=self.__userScript.UserScript()
 			manUnit.setParams(self.__eparameter)
 			manUnit.setInput(self.__iMap)
-			manUnit.manipulate()
+			retVal=manUnit.manipulate()
 			self.__oMap = manUnit.getOutput()
+		return retVal
 
 
 	# write data to output database
