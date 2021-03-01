f52cc1041d1729d08f7d9262847e35a4d284a7f3
bhamacher <b.hamacher@zera.de>
2021-03-01 11:08:26 +0100

    Impulses are a whole number and do not need to be treated differently.
    Removed fomat number from pulses in meter-test conversion.
    
    signed-off-by: bhamacher <b.hamacher@zera.de>

--- zeraconverterengines/MTVisRes.py
+++ zeraconverterengines/MTVisRes.py
@@ -545,7 +545,7 @@ class UserScript:
 
         # eleList.append({"RF" : ""})
         eleList.append({"Cz" :  self.formatNumber(vals["SEC1Module1"]["PAR_DutConstant"])+";"+"x1"+";"+ self.formatNumber(vals["SEC1Module1"]["PAR_DUTConstUnit"])})
-        eleList.append({"M-Puls" :  self.formatNumber(vals["SEC1Module1"]["PAR_MRate"])})
+        eleList.append({"M-Puls" :  str(vals["SEC1Module1"]["PAR_MRate"])})
         eleList.append({"M-Inp" : self.formatNumber(vals["SEC1Module1"]["PAR_DutInput"])})
         eleList.append({"Error" :  self.formatNumber(vals["SEC1Module1"]["ACT_Result"])})
         eleList.append({"N-Value" : ""})
