710b4febd42e2ce94d8c6e59f5d8af178b130f1d
bhamacher <b.hamacher@zera.de>
2021-07-09 11:37:17 +0200

    Fix pylint tab indentation complains.
    Replace tabs with for spaces in library code.
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- pythonconverter_pkg/SqlLite3Interface.py
+++ pythonconverter_pkg/SqlLite3Interface.py
@@ -4,31 +4,31 @@ from sqlite3 import Error
 from pythonconverter_pkg import SqlInterface as zsq
 
 class SqlLiteInterface(zsq.SqlInterface):
-	def __init__(self):
-		print("init sqlite interface")
-		super().__init__()
-	
-	def openDatabase(self,uri):
-		self.file=uri
-		try:
-			self.conn = sqlite3.connect(uri)
-			self.conn.row_factory = sqlite3.Row
-			self.db=self.conn.cursor()
-		except sqlite3.Error:
-			print("db error")
-		return 1
-		
-	def closeDatabase(self):
-		self.conn.close()
+    def __init__(self):
+        print("init sqlite interface")
+        super().__init__()
+    
+    def openDatabase(self,uri):
+        self.file=uri
+        try:
+            self.conn = sqlite3.connect(uri)
+            self.conn.row_factory = sqlite3.Row
+            self.db=self.conn.cursor()
+        except sqlite3.Error:
+            print("db error")
+        return 1
+        
+    def closeDatabase(self):
+        self.conn.close()
 
-	def execute(self, command):
-		self.db.execute(command)
-		ret=self.db.fetchall()
-		print(ret)
-	
-	def save(self):
-		return 0
-		
-	def result(self):
-		return 0
-	
+    def execute(self, command):
+        self.db.execute(command)
+        ret=self.db.fetchall()
+        print(ret)
+    
+    def save(self):
+        return 0
+        
+    def result(self):
+        return 0
+    
