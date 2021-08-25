802e68c0624acf5b77f4406e11c2d68d6eb94643
bhamacher <b.hamacher@zera.de>
2021-08-25 12:40:20 +0200

    Add error codes for several issues
    first 16 bit  (errorRegister variable)
    0: all good
    1: userScript error
    2: open input database error
    4: open output database error
    8: database read error
    16: manipulate set error
    32: write output database error
    64: not used
    128: not used
    256: input database close warning
    512: invalid parameter syntax warning
    1024: session empty or does not exist
    ....
    
    second 16 bit: (userScriptErrors variable)
    0: all good
    1: critical error (not used)
    2: one or more transactions not exported warning
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- pythonconverter_pkg/ConversionUnit.py
+++ pythonconverter_pkg/ConversionUnit.py
@@ -49,38 +49,38 @@ class ConversionUnit:
         return True
 
     def setFilter(self, filter):
-        self.__filter=filter
+        self.__filter = filter
         return True
 
     def setConFile(self, conFile):
         self.__conFile = conFile
         return True
 
-    def seteParam(self,eparams):
+    def seteParam(self, eparams):
+        retVal=True
         try:
             json_acceptable_string = eparams.replace("'", "\"")
             self.__eparameter = json.loads(json_acceptable_string)
         except:
-            self.__eparameter=dict()
-        return True
-    
-    def setUserScript(self,file):
-        retVal=True
-        self.__userScriptPath=file
+            self.__eparameter = dict()
+            self.__errorRegister = self.__errorRegister | (1  << 9)
+            console.log("Bad Parameters. Ignoring!")
+        return retVal
+
+    def setUserScript(self, file):
+        retVal = True
+        self.__userScriptPath = file
         try:
             if file.find('/') != -1:
                 spec = importlib.util.spec_from_file_location("UserScript", self.__userScriptPath)
                 self.__userScript = importlib.util.module_from_spec(spec)
                 spec.loader.exec_module(self.__userScript)
             else:
-                self.__userScript=importlib.import_module(self.__userScriptPath)
+                self.__userScript = importlib.import_module(self.__userScriptPath)
         except:
-            retVal=False
+            self.__errorRegister = self.__errorRegister | (1  << 0)
+            retVal = False
         return retVal
-        
-        
-
-
 
     def ShowSessions(self):
         self.__iInt = self.__dbFact.InputInterface(self.__conType)
@@ -88,19 +88,29 @@ class ConversionUnit:
         ret = self.__iInt.readSessionList()
         return ret
 
-    def convert(self,p_session):
-        retVal=True
+    def convert(self, p_session):
+        retVal = True
         try:
             self.__session = p_session
-            self.__iInt.openDatabase(self.__inputFile)
-            self.__oInt.openDatabase(self.__outputFile)
-            self.__read()
-            retVal=self.__manipulateSet()
-            #print(self.__oMap)
-            self.__write()
-            self.__iInt.closeDatabase()
-        except:
-            retVal=False
+            if not self.__iInt.openDatabase(self.__inputFile):
+                self.__errorRegister = self.__errorRegister | (1  << 1)
+                raise Exception("Fatal Error")
+            if not self.__oInt.openDatabase(self.__outputFile):
+                self.__errorRegister = self.__errorRegister | (1  << 2)
+                raise Exception("Fatal Error")
+            if not self.__read():
+                self.__errorRegister = self.__errorRegister | (1  << 3)
+                raise Exception("Fatal Error")
+            if not self.__manipulateSet():
+                self.__errorRegister = self.__errorRegister | (1  << 4)
+                raise Exception("Fatal Error")
+            if not self.__write():
+                self.__errorRegister = self.__errorRegister | (1  << 5)
+                raise Exception("Fatal Error")
+            if not self.__iInt.closeDatabase():
+                self.__errorRegister = self.__errorRegister | (1  << 8)
+        except Exception as error:
+            retVal = False
         return retVal
 
     def __readTransactionList(self):
@@ -114,6 +124,8 @@ class ConversionUnit:
         retVal = True
         try:
             transList = self.__readTransactionList()
+            if len(transList) is 0:
+                self.__errorRegister = self.__errorRegister | (1  << 10)
             self.__iMap[self.__session] = dict()
             self.__iMap[self.__session]["dynamic"] = dict()
             self.__iMap[self.__session]["static"] = dict()
