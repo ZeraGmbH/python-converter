025f4bcddc8be6ac033fb8b6daff3d2de81f8fed
bhamacher <b.hamacher@zera.de>
2021-09-02 14:10:54 +0200

    Add unittests for com5003
    Adding a database for each session
    and add unittest for only gui, custom and all data usecase
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- unittesting/CppUnittest.py
+++ unittesting/CppUnittest.py
@@ -2,7 +2,7 @@ import unittest
 from pythonconverter_pkg import CppInterface
 
 class TestCppInterface(unittest.TestCase):
-
+# MT Tests
     def test_mtDatabaseConversionGuiOnly(self):
         CppInterface.setInputPath("test/mttest.db")
         CppInterface.setOutputPath("test/result.xml")
@@ -29,6 +29,93 @@ class TestCppInterface(unittest.TestCase):
 
     def test_mtDatabaseConversionAll(self):
         CppInterface.setInputPath("test/mttest.db")
+        CppInterface.setOutputPath("test/result.xml")
+        CppInterface.setSession("all")
+        CppInterface.setEngine("zeraconverterengines.MTVisRes")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+        CppInterface.setOutputPath("test/main.xml")
+        CppInterface.setEngine("zeraconverterengines.MTVisMain")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+# Com Tests
+    #Meas Session
+    def test_comMeasDatabaseConversionGuiOnly(self):
+        CppInterface.setInputPath("test/comtest_meas.db")
+        CppInterface.setOutputPath("test/result.xml")
+        CppInterface.setSession("guionly")
+        CppInterface.setEngine("zeraconverterengines.MTVisRes")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+        CppInterface.setOutputPath("test/main.xml")
+        CppInterface.setEngine("zeraconverterengines.MTVisMain")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+
+    def test_comMeasDatabaseConversionCustom(self):
+        CppInterface.setInputPath("test/comtest_meas.db")
+        CppInterface.setOutputPath("test/result.xml")
+        CppInterface.setSession("custom")
+        CppInterface.setEngine("zeraconverterengines.MTVisRes")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+        CppInterface.setOutputPath("test/main.xml")
+        CppInterface.setEngine("zeraconverterengines.MTVisMain")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+
+    # def test_comMeasDatabaseConversionAll(self):
+    #     CppInterface.setInputPath("test/comtest_meas.db")
+    #     CppInterface.setOutputPath("test/result.xml")
+    #     CppInterface.setSession("all")
+    #     CppInterface.setEngine("zeraconverterengines.MTVisRes")
+    #     CppInterface.setFilter("Snapshot")
+    #     self.assertEqual(CppInterface.convert(),0)
+    #     CppInterface.setOutputPath("test/main.xml")
+    #     CppInterface.setEngine("zeraconverterengines.MTVisMain")
+    #     CppInterface.setFilter("Snapshot")
+    #     self.assertEqual(CppInterface.convert(),0)
+
+    #Ref Session
+    def test_comRefDatabaseConversionGuiOnly(self):
+        CppInterface.setInputPath("test/comtest_ref.db")
+        CppInterface.setOutputPath("test/result.xml")
+        CppInterface.setSession("gui")
+        CppInterface.setEngine("zeraconverterengines.MTVisRes")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+        CppInterface.setOutputPath("test/main.xml")
+        CppInterface.setEngine("zeraconverterengines.MTVisMain")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+
+    #CED Session
+    def test_comCedDatabaseConversionGuiOnly(self):
+        CppInterface.setInputPath("test/comtest_ced.db")
+        CppInterface.setOutputPath("test/result.xml")
+        CppInterface.setSession("gui")
+        CppInterface.setEngine("zeraconverterengines.MTVisRes")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+        CppInterface.setOutputPath("test/main.xml")
+        CppInterface.setEngine("zeraconverterengines.MTVisMain")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+
+    def test_comCedDatabaseConversionCustom(self):
+        CppInterface.setInputPath("test/comtest_ced.db")
+        CppInterface.setOutputPath("test/result.xml")
+        CppInterface.setSession("custom")
+        CppInterface.setEngine("zeraconverterengines.MTVisRes")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+        CppInterface.setOutputPath("test/main.xml")
+        CppInterface.setEngine("zeraconverterengines.MTVisMain")
+        CppInterface.setFilter("Snapshot")
+        self.assertEqual(CppInterface.convert(),0)
+
+    def test_comCedDatabaseConversionAll(self):
+        CppInterface.setInputPath("test/comtest_ced.db")
         CppInterface.setOutputPath("test/result.xml")
         CppInterface.setSession("all")
         CppInterface.setEngine("zeraconverterengines.MTVisRes")
